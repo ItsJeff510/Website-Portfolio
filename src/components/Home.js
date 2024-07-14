d8bc1142e89732d02225c845a9e9f817c5890f4b
Jovanny Ramos <jovannyr72@gmail.com>
2024-07-13 18:24:26 -0700

    Updating landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,18 @@
+import React from 'react'
+// import Navbar from './Navbar'
 
+const home = () => {
+  return (
+    <div className='home-container'>
+        <div className='link-container'>
+            <a className='box' href="google.com"><div className='box'>A</div></a>
+            <a className='box' href="google.com"><div className='box'>B</div></a>
+            <a className='box' href="google.com"><div className='box'>C</div></a>
+            <a className='box' href="google.com"><div className='box'>D</div></a>
+        </div>
+
+    </div>
+  )
+}
+
+export default home
